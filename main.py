75f5a493678835ed454081db6935cb2cac696670
KaptainMidnight <35865450+KaptainMidnight@users.noreply.github.com>
2019-01-24 19:03:01 +0400

    Made some changes

--- main.py
+++ main.py
@@ -1,31 +1,38 @@
-import json
+import json  # Import module json
 
-filename = "settings.json"
-myfile = open(filename, 'w', encoding="Latin-1")
-
-buyer_1 = {
+'''Create file and open him'''
+filename = "settings.json"  # Create json file
+myfile = open(filename, 'w', encoding="Latin-1")  # Open file settings.json
+'''Create dictionary'''
+customer_1 = {  # First customer and his basket
     "name": "Adidas",
 }
 
-buyer_2 = {
+customer_2 = {  # Second customer and his basket
     "name": "Nike"
 }
 
-buyer_3 = {
+customer_3 = {  # Third customer and his backet
     "name": "Adidas",
     "name_2": "Nike"
 }
 
-buyer_4 = {
+customer_4 = {  # Fourth customer and his basket
     "name": "Adidas",
     "name_2": "Reebok"
 }
-
-sells = []
-sells.append(buyer_1)
-sells.append(buyer_2)
-sells.append(buyer_3)
-sells.append(buyer_4)
+'''Insert in list sells'''
+choice = []  # Choice customers
+choice.append(customer_1)  # Add in list first customer with his basket
+choice.append(customer_2)  # Add in list second customer with his basket
+choice.append(customer_3)  # Add in list third customer with his basket
+choice.append(customer_4)  # Add in list fourth customer with his basket
 #  Save JSON
-json.dump(sells, myfile, indent=2, ensure_ascii=False)
-myfile.close()
+json.dump(choice, myfile, indent=2, ensure_ascii=False)  # Save in file settings.json
+myfile.close()  # Close file
+
+#  Read and Load file settings.json
+
+my_json = open(filename, 'r', encoding='utf-8')  # Open file settins.json
+json_data = json.load(my_json)  # Load file settings.json in variable json_data
+print(data_json)  # Print variable json_data
